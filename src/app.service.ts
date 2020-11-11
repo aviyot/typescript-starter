8cbb67c868ab49a186d8118316bc2d0d3f987acf
aviyot <aviyot@gmail.com>
2020-11-11 10:43:59 +0200

    response json

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hellooooooo World! from aviyot getahun';
+  getHello(): any {
+    return {fristname:"aviyot",lastname:"getahun"};
   }
 }
